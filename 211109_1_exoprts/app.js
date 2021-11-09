ce8ddde0eba5a1914420cfc76b55761cfc1d6381
Alfred <ya95123@gmail.com>
2021-11-09 17:41:33 +0800

    exoprts

---
+++ 211109_1_exoprts/app.js
@@ -0,0 +1,8 @@
+// 引入
+const content = require("./data")
+
+let a = 1
+
+console.log(a)
+console.log(content)
+// console.log(content.bark())
---
+++ 211109_1_exoprts/data.js
@@ -0,0 +1,11 @@
+let data = 2
+
+// *模組輸出
+module.exports = {
+  "data": data
+}
+// *個別輸出，會被包在 json{}，上下寫法不可共用
+// exports.data = 2
+// exports.bark = () => {
+//   return "bark!!"
+// }
