3841efad7de44ca504f7e396bd1c6dc2d6fa94f9
Alfred <ya95123@gmail.com>
2021-11-10 17:56:57 +0800

    debug

---
+++ 211110_3_debug/app.js
@@ -0,0 +1,7 @@
+const dogStatus = require("./dogStatus")
+console.log(dogStatus)
+
+let num = 3
+debugger
+
+dogStatus.bark(3)
---
+++ 211110_3_debug/dogStatus.js
@@ -0,0 +1,9 @@
+const bark = (num) => {
+  let dogneme = "tom"
+  let dogAge = 2
+  console.log(`汪 ${num} 次`)
+}
+
+module.exports = {
+  "bark": bark
+}
