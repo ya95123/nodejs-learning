8687e7c10d7890fd32a527f7cde6fe8dc311acc8
Alfred <ya95123@gmail.com>
2021-11-12 11:59:38 +0800

    ref / set / onValue / get

--- 211111_1_firebase/js/firebase.js
+++ 211111_1_firebase/js/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js"
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-analytics.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js"
+import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,8 +22,44 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
-const database = getDatabase(app)
+const db = getDatabase(app)
 
 console.log(app)
 console.log(analytics)
-console.log(database)
+console.log(db)
+
+// *ref() 尋找資料庫路徑
+// *set() 新增資料
+// *firebase 全部物件格式，不能陣列內容
+
+// *寫入資料
+// set(ref(db), {
+//   "student001": {
+//     "username": "Alfred",
+//     "age": 26,
+//     "sex": "male",
+//     "country": "Taiwan"
+//   },
+//   "student002": {
+//     "username": "Mark",
+//     "age": 27,
+//     "sex": "male",
+//     "country": "USA"
+//   }
+// })
+
+// set(ref(db, "student002/age"), 25)
+
+// *讀取資料
+// *onValue() 監聽數據變化
+const student001 = ref(db, "student001")
+// js 調用值時用 val()
+onValue(student001, (snapshot) => {
+  console.log(snapshot.val())
+})
+// * get()，讀取數據一次，詳情看文檔
+get(student001).then(snapshot => {
+  console.log(snapshot.val())
+}).catch(err => {
+  console.log(err)
+})
